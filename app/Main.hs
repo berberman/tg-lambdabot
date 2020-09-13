f781013b9b229d0ba18b9afb115ce19402c24b75
berberman <1793913507@qq.com>
2020-09-13 08:04:32 +0800

    Run forever.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Control.Exception (SomeException, catch)
 import Lib
 
 main :: IO ()
-main = runApplication
+main = runApplication `catch` (\(e :: SomeException) -> print e >> runApplication)
